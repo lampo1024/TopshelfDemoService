40cd3fdc4a8f5657fc1a93666908484aba1ff436
lampo1024 <lampo1024@gmail.com>
2019-04-11 11:01:25 +0800

    Solve session 0 isolation problem

--- TopshelfDemoService/ProcessorHelper.cs
+++ TopshelfDemoService/ProcessorHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
@@ -47,6 +48,10 @@ namespace TopshelfDemoService
         {
             try
             {
+                ProcessExtensions.StartProcessAsCurrentUser(applicationPath, args);
+            }
+            catch (Exception e)
+            {
                 var psi = new ProcessStartInfo
                 {
                     FileName = applicationPath,
@@ -55,7 +60,6 @@ namespace TopshelfDemoService
                 };
                 Process.Start(psi);
             }
-            catch{}
         }
     }
 }
